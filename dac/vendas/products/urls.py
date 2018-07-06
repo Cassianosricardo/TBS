5a378931f269851bd1cc57713c311f0d1a6c7eaf
Cassiano Ricardo Santana da Cruz Filho <cassiano.sricardo@gmail.com>
2018-07-06 08:32:19 -0300

    Lógica Produtos

---
+++ dac/vendas/products/urls.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.urls import path
+from .views import list_products
+
+urlpatterns = [
+    path('', list_products, name='list_products'),
+]
---
+++ dac/vendas/products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.shortcuts import render
+import requests
+from requests.auth import HTTPBasicAuth
+
+def list_products(request):    
+    response = requests.get('http://172.19.0.4:8000/list')        
+    data = response.json()    
+    print (data)
+    return render(request, 'products.html', {'data':data})    
